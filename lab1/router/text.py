cd455a1f364f69378a88c86b8fd572d654b1be63
Zax4r <zaharbedarik@gmail.com>
2026-02-07 19:02:57 +0300

    now updates cookies with current file

--- lab1/router/text.py
+++ lab1/router/text.py
@@ -1,21 +1,25 @@
 from database import SessionDep
 from schemas.lemma import SLemmaAdd, SLemmaUpdate, SLemmaResponse
 from schemas.text import STextAdd, STextUpdate, STextResponse
-from fastapi import APIRouter, status, HTTPException, UploadFile
+from fastapi import APIRouter, status, HTTPException, UploadFile, Response
 from repository import LemmaRepository, TextRepository
 from processing import DocReader
 
 
 router = APIRouter(prefix="/text", tags=["Тексты"])
 
-
 @router.get("/{id}", response_model=STextResponse, status_code=status.HTTP_200_OK)
-async def get_text(id: int, session: SessionDep):
+async def get_text(id: int, session: SessionDep, response: Response):
     text = await TextRepository.get_one(id, session)
     if text is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail=f"Text with id {id} not found"
         )
+    
+    response.set_cookie(
+        key="current_text_id", 
+        value=str(text.id))
+    
     return text
 
 @router.post('/add',response_model=STextResponse, status_code=status.HTTP_201_CREATED)
